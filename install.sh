55f57985fedc473f569659e11c0a543e9836d38b
Zhong Lufan <lufanzhong@gmail.com>
2023-12-02 04:34:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ SERVICE_PATH=$HOME/.config/systemd/user/wsl2-ssh-agent-shell.service
 sed "s|<exec>|$PWD/wsl2-ssh-agent.sh|" wsl2-ssh-agent.service.in | install -D -m 644 -v /dev/stdin "$SERVICE_PATH"
 
 echo
-echo -e "\033[1m${SERVICE_PATH}e:\033[0m"
+echo -e "\033[1m${SERVICE_PATH}:\033[0m"
 echo 
 cat "$SERVICE_PATH"
 
